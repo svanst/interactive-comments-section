0bcd2b3cd96cfa4cd9a206562a846e97c7e67aaa
svanst <144538702+svanst@users.noreply.github.com>
2024-08-03 15:01:19 +0200

    add button and icon component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,60 @@
 import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
 import MaxWidthWrapper from "./MaxWidthWrapper/MaxWidthWrapper";
+import Button from "./Button/Button";
+import {
+  IconDelete,
+  IconEdit,
+  IconMinus,
+  IconPlus,
+  IconReply,
+} from "./Icon/Icon";
 
 export default function App() {
   return (
     <main>
       <MaxWidthWrapper>
-        <VisuallyHidden asChild>
-          <h1>Interactive comments section</h1>
-        </VisuallyHidden>
-        <h1>Hello there</h1>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            gap: "1rem",
+          }}
+        >
+          <VisuallyHidden asChild>
+            <h1>Interactive comments section</h1>
+          </VisuallyHidden>
+          <Button variant="ghost">
+            <IconReply /> Reply
+          </Button>
+          <Button color="primary-light" variant="icon">
+            <IconPlus />
+          </Button>
+          <Button color="primary-light" variant="icon">
+            <IconMinus />
+          </Button>
+          <Button color="danger" variant="ghost">
+            <IconDelete /> Delete
+          </Button>
+          <Button color="primary" variant="fill">
+            Send
+          </Button>
+          <Button color="neutral" variant="fill">
+            No, cancel
+          </Button>
+          <Button color="danger" variant="fill">
+            Yes, delete
+          </Button>
+
+          <Button color="primary-light" variant="fill">
+            Yes, delete
+          </Button>
+        </div>
       </MaxWidthWrapper>
     </main>
   );
 }
+
+const styles = {
+  test: "test",
+};
---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,18 @@
+import classNames from "classnames";
+import styles from "./Button.module.css";
+
+function Button({ children, className, variant, color = "primary", ...rest }) {
+  const combinedClasses = classNames(
+    styles.btn,
+    styles[`btn--${variant}`],
+    styles[`btn--${color}`],
+    className
+  );
+  return (
+    <button className={combinedClasses} {...rest}>
+      {children}
+    </button>
+  );
+}
+
+export default Button;
